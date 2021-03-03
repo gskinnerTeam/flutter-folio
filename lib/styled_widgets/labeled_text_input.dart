27ea18e53dd61e7bd26268be8aba380920762f4a
Shawn <shawn@gskinner.com>
2021-03-03 11:11:19 -0700

    Fix for linux textfields on dev 1.27

--- lib/styled_widgets/labeled_text_input.dart
+++ lib/styled_widgets/labeled_text_input.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_folio/_utils/string_utils.dart';
 import 'package:flutter_folio/core_packages.dart';
 import 'package:flutter_folio/models/app_model.dart';
+import 'package:universal_platform/universal_platform.dart';
 
-class LabeledTextInput extends StatelessWidget {
+class LabeledTextInput extends StatefulWidget {
   const LabeledTextInput({
     Key key,
     this.text,
@@ -32,6 +34,17 @@ class LabeledTextInput extends StatelessWidget {
   final bool obscureText;
 
   @override
+  _LabeledTextInputState createState() => _LabeledTextInputState();
+}
+
+class _LabeledTextInputState extends State<LabeledTextInput> {
+  String lastValue;
+
+  int lastPosition;
+
+  final FocusNode rawFocus = FocusNode();
+
+  @override
   Widget build(BuildContext context) {
     AppTheme theme = context.watch();
     bool touchMode = context.select((AppModel m) => m.enableTouchMode);
@@ -41,52 +54,81 @@ class LabeledTextInput extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          if (StringUtils.isNotEmpty(label)) ...[
-            Text(label ?? "", style: labelStyle ?? TextStyles.caption),
+          if (StringUtils.isNotEmpty(widget.label)) ...[
+            Text(widget.label ?? "", style: widget.labelStyle ?? TextStyles.caption),
             VSpace.med,
           ],
           TweenAnimationBuilder<double>(
             tween: Tween(begin: verticalPadding, end: verticalPadding),
             duration: Times.fast,
             curve: Curves.easeOut,
-            builder: (_, animatedPaddingVt, __) => TextFormField(
-              autofillHints: autofillHints,
-              controller: controller,
-              onChanged: onChanged,
-              onFieldSubmitted: onSubmit,
-              initialValue: text,
-              style: style ?? TextStyles.body2,
-              autofocus: true,
-              minLines: numLines,
-              maxLines: numLines,
-              obscureText: obscureText ?? false,
-              decoration: InputDecoration(
-                  hintText: hintText ?? "",
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: Corners.smBorder,
-                      borderSide: BorderSide(
-                        color: theme.greyWeak,
-                        width: 1,
-                        style: BorderStyle.solid,
-                      )),
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: Corners.smBorder,
-                      borderSide: BorderSide(
-                        color: theme.accent1,
-                        width: 1,
-                        style: BorderStyle.solid,
-                      )),
-                  contentPadding: EdgeInsets.only(
-                    left: Insets.med,
-                    right: Insets.med,
-                    top: animatedPaddingVt,
-                    bottom: animatedPaddingVt - 2,
-                  ),
-                  isDense: true),
-            ),
+            builder: (_, animatedPaddingVt, __) {
+              return RawKeyboardListener(
+                focusNode: rawFocus,
+                onKey: (value) {
+                  lastPosition = widget.controller.selection.start;
+                },
+                child: TextFormField(
+                  autofillHints: widget.autofillHints,
+                  controller: widget.controller,
+                  onChanged: UniversalPlatform.isLinux ? _fixTextOnLinux : widget.onChanged,
+                  onFieldSubmitted: widget.onSubmit,
+                  initialValue: widget.text,
+                  style: widget.style ?? TextStyles.body2,
+                  autofocus: true,
+                  minLines: widget.numLines,
+                  maxLines: widget.numLines,
+                  obscureText: widget.obscureText ?? false,
+                  decoration: InputDecoration(
+                      hintText: widget.hintText ?? "",
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: Corners.smBorder,
+                          borderSide: BorderSide(
+                            color: theme.greyWeak,
+                            width: 1,
+                            style: BorderStyle.solid,
+                          )),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: Corners.smBorder,
+                          borderSide: BorderSide(
+                            color: theme.accent1,
+                            width: 1,
+                            style: BorderStyle.solid,
+                          )),
+                      contentPadding: EdgeInsets.only(
+                        left: Insets.med,
+                        right: Insets.med,
+                        top: animatedPaddingVt,
+                        bottom: animatedPaddingVt - 2,
+                      ),
+                      isDense: true),
+                ),
+              );
+            },
           ),
         ],
       ),
     );
+  }
+
+  void _fixTextOnLinux(String value) {
+    if (value.length > 1 && lastValue.length < value.length) {
+      final enteredChar = value[0];
+      final oldValue = value.substring(1);
+
+      final prefix = oldValue.substring(0, lastPosition);
+      final suffix = oldValue.substring(lastPosition);
+      final newValue = prefix + enteredChar + suffix;
+      widget.controller.value = TextEditingValue(
+        text: newValue,
+        selection: TextSelection.collapsed(offset: lastPosition + 1),
+      );
+      lastValue = newValue;
+      lastPosition = lastPosition + 1;
+    } else {
+      lastValue = value;
+      lastPosition = value.length;
+    }
+    widget.onChanged(value);
   }
 }
