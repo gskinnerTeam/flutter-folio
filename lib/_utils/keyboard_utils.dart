16bcdfafc5cac68663c9c7a9a5aab033e3b4d9c3
Shawn <shawn@gskinner.com>
2021-05-16 23:17:19 -0600

    Improve isKeyDown check

--- lib/_utils/keyboard_utils.dart
+++ lib/_utils/keyboard_utils.dart
@@ -1,22 +1,15 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_folio/_utils/device_info.dart';
 
 class KeyboardUtils {
-  static bool get isSpanSelectModifierDown => isKeyDown([LogicalKeyboardKey.shiftLeft, LogicalKeyboardKey.shiftRight]);
-  static bool get isControlDown => isKeyDown([LogicalKeyboardKey.controlLeft, LogicalKeyboardKey.controlRight]);
-  static bool get isMetaDown => isKeyDown([LogicalKeyboardKey.metaLeft, LogicalKeyboardKey.metaRight]);
+  static bool get isSpanSelectModifierDown => isKeyDown({LogicalKeyboardKey.shiftLeft, LogicalKeyboardKey.shiftRight});
+  static bool get isControlDown => isKeyDown({LogicalKeyboardKey.controlLeft, LogicalKeyboardKey.controlRight});
+  static bool get isMetaDown => isKeyDown({LogicalKeyboardKey.metaLeft, LogicalKeyboardKey.metaRight});
   static bool get isWindowsDown => isMetaDown;
   static bool get isCommandDown => isMetaDown;
 
-  static bool isKeyDown(List<LogicalKeyboardKey> keys) {
-    Set<LogicalKeyboardKey> pressed = RawKeyboard.instance.keysPressed;
-    for (var i = keys.length; i-- > 0;) {
-      if (pressed.contains(keys[i])) {
-        return true;
-      }
-    }
-    return false;
+  static bool isKeyDown(Set<LogicalKeyboardKey> keys) {
+    return keys.intersection(RawKeyboard.instance.keysPressed).isNotEmpty;
   }
 
   static bool get isMultiSelectModifierDown {
