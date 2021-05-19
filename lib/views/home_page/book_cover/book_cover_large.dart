bcb65e6846221c540d30a557b1bb24ff6fed022c
Shawn <shawn@gskinner.com>
2021-05-19 10:09:29 -0600

    improve sizing logic for inlineTextEditor

--- lib/views/home_page/book_cover/book_cover_large.dart
+++ lib/views/home_page/book_cover/book_cover_large.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:animate_do/animate_do.dart';
 import 'package:context_menus/context_menus.dart';
 import 'package:flutter/foundation.dart';
@@ -20,70 +22,72 @@ class LargeBookCover extends StatelessWidget {
     AppTheme theme = context.watch();
     // Collapse padding a bit on short views
     double paddingScale = context.heightPx < 650 ? .5 : 1;
-    return FadeInLeft(
-      delay: Duration(milliseconds: 500),
-      child: ConstrainedBox(
-        constraints: BoxConstraints(maxWidth: 510),
-        child: FocusTraversalGroup(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              /// Title
-              InlineTextEditor(
-                book.title,
-                maxLines: 2,
-                maxLength: 22,
-                onFocusOut: _handleTitleEditingEnded,
-                promptText: "Add Title",
-                key: ValueKey("title" + book.title),
-                width: 590,
-                style: TextStyles.h1.copyWith(color: theme.surface1),
-              ),
-              VSpace.med,
+    return LayoutBuilder(
+      builder: (_, constraints) => FadeInLeft(
+        delay: Duration(milliseconds: 500),
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxWidth: 510),
+          child: FocusTraversalGroup(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                /// Title
+                InlineTextEditor(
+                  book.title,
+                  maxLines: 2,
+                  maxLength: 22,
+                  onFocusOut: _handleTitleEditingEnded,
+                  promptText: "Add Title",
+                  key: ValueKey("title" + book.title),
+                  width: min(constraints.maxWidth, 550),
+                  style: TextStyles.h1.copyWith(color: theme.surface1),
+                ),
+                VSpace.med,
 
-              /// Meta-Data (page count, last edited, etc)
-              UiText.rich(TextSpan(children: [
-                TextSpan(
-                    text: "${book.pageCount} page${book.pageCount == 1 ? "" : "s"}, ",
-                    style: TextStyles.title1.copyWith(color: theme.surface1)),
-                TextSpan(
-                    text: " edited ${timeago.format(book.getLastModifiedDate())}",
-                    style: TextStyles.title2.copyWith(color: theme.grey))
-              ])),
+                /// Meta-Data (page count, last edited, etc)
+                UiText.rich(TextSpan(children: [
+                  TextSpan(
+                      text: "${book.pageCount} page${book.pageCount == 1 ? "" : "s"}, ",
+                      style: TextStyles.title1.copyWith(color: theme.surface1)),
+                  TextSpan(
+                      text: " edited ${timeago.format(book.getLastModifiedDate())}",
+                      style: TextStyles.title2.copyWith(color: theme.grey))
+                ])),
 
-              /// Thick Divider
-              VSpace(36 * paddingScale),
-              Container(color: Colors.deepOrange, width: 32, height: 4),
-              VSpace.sm,
+                /// Thick Divider
+                VSpace(36 * paddingScale),
+                Container(color: Colors.deepOrange, width: 32, height: 4),
+                VSpace.sm,
 
-              /// Desc
-              InlineTextEditor(book.desc,
-                  key: ValueKey("desc" + book.desc),
-                  promptText: "Add Description",
-                  onFocusOut: _handleDescEditingEnded,
-                  width: 370,
-                  // SB: Set web to 1 instead of 1.8, it was causing rendering issues where the text would get cut-off.
-                  // TODO: Log bug on this ^
-                  style: TextStyles.body1.copyWith(height: kIsWeb ? 1 : 1.8, color: theme.greyWeak),
-                  maxLength: 30 * 3,
-                  maxLines: 3),
-              VSpace(Insets.xl * 1.2 * paddingScale),
+                /// Desc
+                InlineTextEditor(book.desc,
+                    key: ValueKey("desc" + book.desc),
+                    promptText: "Add Description",
+                    onFocusOut: _handleDescEditingEnded,
+                    width: 300,
+                    // SB: Set web to 1 instead of 1.8, it was causing rendering issues where the text would get cut-off.
+                    // TODO: Log bug on this ^
+                    style: TextStyles.body1.copyWith(height: kIsWeb ? 1 : 1.8, color: theme.greyWeak),
+                    maxLength: 30 * 3,
+                    maxLines: 3),
+                VSpace(Insets.xl * 1.2 * paddingScale),
 
-              /// Call to Action Button
-              Row(
-                children: [
-                  ContextMenuRegion(
-                    contextMenu: BookContextMenu(book),
-                    child:
-                        PrimaryBtn(label: "VIEW FOLIO", icon: Icons.chevron_right, onPressed: _handleViewFolioPressed),
-                  ),
-                  HSpace.sm,
-                  StyledSharedBtn(book: book),
-                ],
-              ),
-              Flexible(child: VSpace(Insets.lg + 120 + 80 * paddingScale))
-            ],
+                /// Call to Action Button
+                Row(
+                  children: [
+                    ContextMenuRegion(
+                      contextMenu: BookContextMenu(book),
+                      child: PrimaryBtn(
+                          label: "VIEW FOLIO", icon: Icons.chevron_right, onPressed: _handleViewFolioPressed),
+                    ),
+                    HSpace.sm,
+                    StyledSharedBtn(book: book),
+                  ],
+                ),
+                Flexible(child: VSpace(Insets.lg + 120 + 80 * paddingScale))
+              ],
+            ),
           ),
         ),
       ),
